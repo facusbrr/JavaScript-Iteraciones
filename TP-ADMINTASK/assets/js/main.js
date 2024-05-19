5d109091df6214f7bd4104d2e9e723b1fed7ef30
Gayoso Facundo <facusbrr@gmail.com>
2024-05-19 16:30:18 -0300

    Se realizó el menú y la función de eliminar

--- TP-ADMINTASK/assets/js/main.js
+++ TP-ADMINTASK/assets/js/main.js
@@ -2,21 +2,74 @@ let lista = [];
 
 function agregarTarea(nuevaTarea) {
   if (nuevaTarea.trim() === "") {
-    console.log("No puede estar vacío");
-  return;
+    alert("No puede estar vacío");
   } else {
-    lista.push(nuevaTarea)
-    console.log(`Tarea "${nuevaTarea}" se puso correctamente`);
+    lista.push(nuevaTarea);
+    alert(`Tarea "${nuevaTarea}" se agregó correctamente`);
   }
 }
 
-function listarTarea(){
+function listarTarea() {
+  if (lista.length === 0) {
+    alert("La lista está vacía");
+  } else {
+    let tareas = "Tareas: \n";
+    for (let i = 0; i < lista.length; i++) {
+      tareas += `${i + 1}- ${lista[i]}\n`
+    }
+    alert(tareas)
+  }
+}
+
+function eliminarTarea(){
   if (lista.length === 0){
-    console.log ("La lista está vacía")
+    alert("Está vacía");
   return;
-  }else{
-    for (let i = 0; i < lista.length; i++){
-        console.log (`${i + 1}- ${lista[i]}`);
+  }
+    listarTarea()
+    let tareaEliminar = prompt("Ingrese el nombre de la tarea a eliminar: ");
+    let indice = lista.indexOf(tareaEliminar);
+
+    if (indice !== -1){
+      lista.splice(indice, 1);
+      alert(`Tarea "${tareaEliminar}" eliminada correctamente`);
+    } else {
+      alert(`No se encuentra la tarea`);
+    }
+  }
+
+function menu() {
+  let opcion = 1;
+
+  while (opcion !== 0) {
+
+    opcion = parseInt(prompt(` Menu:
+    1- Agregar Tarea
+    2- Listar Tarea
+    3- Eliminar Tarea
+    4- Editar Tarea
+    0- Salir
+    
+    Seleccione una opción: `));
+
+    switch (opcion) {
+      case 1:
+        agregarTarea(prompt("Ingresar el nombre de la nueva tarea: "));
+        break;
+      case 2:
+        listarTarea();
+        break;
+      case 3:
+        eliminarTarea();
+        break;
+      case 0:
+        alert("Gracias por probar el código");
+        break;
+      default:
+        alert("Opción incorrecta");
+        break;
     }
   }
 }
+
+menu();
